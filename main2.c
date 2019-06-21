60f6a7d9e273a6d8fa3b916879674d4cfe2d64b4
GabrielCastroSilva <42621099+GabrielCastroSilva@users.noreply.github.com>
2019-06-21 13:10:04 -0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 struct Node
 {
@@ -14,8 +15,12 @@ typedef struct Node NODO;
 
 // prototipos
 void inserir(NODO** arv, int val, char c);
-void mostrar(NODO* arv);
 int pesquisar(NODO* arv, char caracter, char letras[]);
+
+//variaveis globais
+int freqs[256];
+NODO* listaArvores;
+
 
 // código
 
@@ -46,18 +51,6 @@ void inserir(NODO** arv, int val, char c)
 
 
 
-void mostrar(NODO* arv)
-{
-    if (arv == NULL)
-        return;
-
-    printf("#(%c,%d", arv->let, arv->valor);
-    mostrar(arv->esq);
-    mostrar(arv->dir);
-    printf(")");
-
-}
-
 void bubbleSort(NODO* vet[], int count)
 {
     for (int i = 0; i < count; i++)
@@ -74,13 +67,8 @@ void bubbleSort(NODO* vet[], int count)
     }
 }
 
-int freqs[256];
-NODO* listaArvores;
 
 
-/*void insereListaArvores(NODO arv) {
-    listaArvores[tamListaArvores++] = arv;
-}*/
 
 
 void padding ( char ch, int n )
@@ -92,30 +80,24 @@ void padding ( char ch, int n )
 }
 
 
-void printree( NODO* root, int level )  //char** table, char* binary, int column
+void printree( NODO* root, int level )
 {
     int i;
-    // Change to test for leaf
+
     if(root == NULL)
     {
-        // table[column] = &node->letter;
-        // table[column+1] = binary;
-        // column = column + 2;
         padding ('\t', level);
         puts ("~");
     }
     else
     {
+        // Vai para a direita
         printree(root->dir, level + 1 );
-        // RIGHT
-        // strcat(binary, "1");
-        // gentable(node->right, table, binary, column);
         padding('\t', level );
         printf("%c:%d\n", root->let, root->valor);
+        // Vai para a esquerda
         printree(root->esq, level + 1 );
-        // LEFT
-        // strcat(binary, "0");
-        // gentable(node->left, table, binary, column);
+
     }
 }
 
@@ -151,9 +133,6 @@ int pesquisar(NODO* arv, char character, char c[])
 
 
 
-
-
-
 int main()
 {
     /*Leitura de Arquivo*/
@@ -167,27 +146,28 @@ int main()
         printf("Erro! Arquivo nao encontrado!\n");
         exit(EXIT_FAILURE);
     }
-    // Leitura
 
-    char frase [100];
-    char letra;
 
     char c;
     int freqs[256] = {0};
     int count = 0;
+    char frase [100];
+    int fraseCont = 0;
 
 
     while ((c = fgetc(arq)) != EOF)
     {
-
         freqs[c]++;
+
         if(freqs[c] == 1)
         {
+
             count++;
         }
     }
-    fclose(arq); //Fecha o arquivo
 
+
+    fclose(arq); //Fecha o arquivo
 
 
     int tamArvores = 0;
@@ -204,7 +184,8 @@ int main()
 
         }
     }
-
+///////////////////////////////////////////////////////////////////////
+//Prints dos elementos nao ordenados e ordenados
     printf("lista arvores nao ordenado...\n");
     for(int i=0; i<tamArvores; i++)
     {
@@ -220,20 +201,18 @@ int main()
         printf("(%c:%d)", listaArvores[i]->let, listaArvores[i]->valor);
     }
     printf("\n");
+///////////////////////////////////////////////////////////////////////////
 
 
     NODO* listaArvoresOLD[tamArvores];
-    for(int i=0; i<tamArvores; i++)
+    for(int i=0; i<tamArvores; i++) //Clone da arvore atual antes das operacoes realizadas
     {
         listaArvoresOLD[i] = listaArvores[i];
     }
 
 
 
-
-    //arrayIntoTree();
-
-    for(int i = 0  ; i<tamArvores - 1  ; i++)
+    for(int i = 0  ; i<tamArvores - 1  ; i++)   //Arvore e montada
     {
         NODO* t = NULL;
         inserir(&t, listaArvores[tamArvores - i - 1]->valor + listaArvores[tamArvores - i - 2]->valor, 'Z' );
@@ -241,12 +220,12 @@ int main()
         t->dir = listaArvores[tamArvores - i - 2];
         listaArvores[tamArvores - i - 2] = t;
         bubbleSort(listaArvores,tamArvores);
-
-
     }
     printree(listaArvores[0],0);
 //////////////////////////////////////////////////////////////////////////////////
-    FILE* writtenArq = fopen("tabela.txt", "w");
+
+
+    FILE* writtenArq = fopen("tabela.txt", "w"); //Arquivo de escrita
     if(writtenArq == NULL)
     {
         printf("Impossível criar arq!\n");
@@ -254,18 +233,31 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    for(int i = 0; i < tamArvores; i++)
+    for(int i = 0; i < tamArvores; i++) //Escreve os caracteres e seus respectivos binarios
     {
         char c[] = {0};
         pesquisar(*listaArvores,listaArvoresOLD[i]->let, c);
-        fprintf(arq, "\nCaracter: %c\tBinario: %s", listaArvoresOLD[i]->let,strrev(c));
+        fprintf(arq, "Caracter: %c\tBinario: %s\n", listaArvoresOLD[i]->let,strrev(c));
     }
+    fprintf(arq, "\nFrase: \t");
+
+
+    FILE* arq2 = fopen("teste.txt", "r");
+
+
+
+    while ((c = fgetc(arq2)) != EOF)  //Escreve a mensagem codificada
+    {
+         char caract[] = {0};
+        pesquisar(*listaArvores,c, caract);
+        fprintf(arq, "%s", strrev(caract));
+
+
+    }
+
 
     fclose(arq);
     printf("\nDados gravados em tabela.txt\n");
 
     return 0;
 }
-
-
-
